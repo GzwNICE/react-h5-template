d832d8885decd532e0a38da9207f39b1488e1b86
lindepeng <648578244@qq.com>
2020-06-08 16:42:34 +0800

    add me

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,14 +1,52 @@
 import React, { PureComponent } from 'react';
+import { Grid } from 'antd-mobile';
 import { connect } from 'react-redux';
 import intl from 'react-intl-universal';
 import TabBarBox from '@/components/tabBar';
 import styles from './index.less';
-
+import authorImg from '@/assets/images/avatar_notlogin.png';
+import ic_gocoin_s from '@/assets/images/ic_gocoin_s.png';
+import goin_arrow from '@/assets/images/personal_ic_arrow@2x.png';
 class User extends PureComponent {
   render() {
+    // eslint-disable-next-line react/destructuring-assignment
+    const tabs = [
+      { label: '待开奖', icon: authorImg },
+      { label: '已中奖', icon: authorImg },
+      { label: '未中奖', icon: authorImg },
+    ];
     return (
       <div>
-        <div className={styles.box}>{intl.get('user.aaa')}</div>
+        <div className={styles.topBox}>
+          <div className={styles.title}>{intl.get('user.title')}</div>
+          <div className={styles.authorInfo}>
+            <img className={styles.authorImg} src={authorImg}></img>
+            <div className={styles.authorLoginType}>
+              <div className={styles.authorName}>{intl.get('user.loginOrRegister')}</div>
+              <div className={styles.authorCoin}>
+                <img className={styles.coin} src={ic_gocoin_s}></img>
+                <span className={styles.label}>{intl.get('user.myGoCoin')}</span>
+                <img className={styles.arrow} src={goin_arrow}></img>
+            </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles.order}>
+            <div>我的订单</div>
+            <Grid
+              data={tabs}
+              columnNum={3}
+              hasLine={false}
+              renderItem={item => (
+                <div>
+                  <img src={item.icon} className={styles.icon} alt="" />
+                  <div className={styles.label}>{item.label}</div>
+                </div>
+              )}
+            />
+          </div>
+        </div>
         <div className={styles.tBar}>
           <TabBarBox selectedTab="userPage" />
         </div>
